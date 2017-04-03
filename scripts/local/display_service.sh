b510088aeddd97e0fe02b95da099ff12a4adc26f
Stéphane Blondon <stephane@yaal.fr>
2017-04-03 13:56:35 +0200

    better display for local webservice

--- scripts/local/display_service.sh
+++ scripts/local/display_service.sh
@@ -1,13 +1,5 @@
 #! /bin/sh
 
-PORT=8000
-if [ `uname` = "Linux" ]
-then
-    IP_ADDRESS=`ip addr | grep -A 2 -E "(enp2s0|eth0)" | grep inet | grep -v inet6  | cut -d " " -f 6 | cut -d "/" -f 1`
-else
-    #IP_ADDRESS=`ifconfig | grep -A 2 "eth0" | grep 'inet adr:' | cut -d " " -f 12  | cut -d ":" -f 2`
-    IP_ADDRESS=`ifconfig | grep -A 5 "en0" | grep 'inet ' | cut -d " " -f 2`
-fi
 
-echo "\tThe web service is available at http://${IP_ADDRESS}:${PORT}"
+env PYTHONPATH=. ./local.virtualenv/bin/python ./scripts/local/env_settings.py
 
--- scripts/local/env_settings.py
+++ scripts/local/env_settings.py
@@ -1,7 +1,20 @@
 import logging
 import os
 
+import colored
+import netifaces
+
 import web.settings
+
+
+def _get_localhost():
+    for interface in netifaces.interfaces():
+        config = netifaces.ifaddresses(interface).values()
+        for param in config:
+            addr = param[0].get("addr")
+            if addr.startswith("192.168"):
+                return addr
+
 
 web.settings.APP_DIR = os.getcwd() + '/'
 web.settings.BIN_DIR = web.settings.APP_DIR
@@ -16,9 +29,13 @@ web.settings.CLIENT_STORAGE = {
 web.settings.LOG_FILE = web.settings.PERSISTENT_DIR + '/logs/wsgi.log'
 web.settings.LOG_LEVEL = logging.DEBUG
 web.settings.PORT = 8000
-web.settings.HOST = 'http://localhost:{0}'.format(web.settings.PORT)
+web.settings.HOST = "http://{}:{}".format(_get_localhost(), web.settings.PORT)
 web.settings.FILES_ROOT_DIR = web.settings.PERSISTENT_DIR + 'files/'
 web.settings.STATISTICS_DIR = web.settings.PERSISTENT_DIR + 'statistics/'
 web.settings.NOTIFY_ON_500 = False
 web.settings.NOTIFY_AUTHOR = web.settings.APP_NAME + '-local'
+
+if __name__ == '__main__':
+    info = "\tThe web service is available at " + web.settings.HOST
+    print(colored.stylize(info, colored.fg("green")))
 
